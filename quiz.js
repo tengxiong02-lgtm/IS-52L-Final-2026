92a783d8ac35b4488a3149bdf1bf37cf5e875e63
tengxiong02 <tengxiong02@mail.fresnostate.edu>
2026-05-11 23:58:57 -0700

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quiz = [
+
+{
+  chapter: "Chapter 1 Quiz",
+  id: 1,
+
+  question: "What does RAM temporarily store?",
+
+  answers: [
+    "Permanent system files",
+    "Currently used programs and data",
+    "Deleted files",
+    "Internet passwords"
+  ],
+
+  correct: 1,
+
+  explanation: "RAM stores the programs and data currently being used by the computer while it is running."
+}
+
+];
